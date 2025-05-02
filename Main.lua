269c625b54597a723069e4c0a515c4c032e8f0d2
Lemon251206 <64097234+godcraft998@users.noreply.github.com>
2025-05-02 07:38:44 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -218,6 +218,13 @@ PlayerGui.ChildAdded:Connect(function(child)
     end
 end)
 
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function() 
+    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame) 
+    wait(1) 
+    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
 for i = 1, 6 do
     local slot = tostring(i);
     local unitName = GetUnitLoadout(slot);
